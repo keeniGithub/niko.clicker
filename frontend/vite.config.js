a647e3089cb0ac2f7df2e128994da6c6d574dff3
ItzKeeni <maratkanovartem89@mail.ru>
2025-07-31 22:39:26 +0300

    fix pls я заебался

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsInlineLimit: 0
-  },
-})
+export default {
+  plugins: [netlify()],
+};
